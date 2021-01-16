bf8c7863b9a179e183c39187f1a56814abd77856
AnastasiaPolkovnikova <68498874+AnastasiaPolkovnikova@users.noreply.github.com>
2021-01-16 16:31:42 +0800

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -13,14 +13,14 @@ let path = {
     html: [source_folder + '/*.html', "!" + source_folder + "/_*.html"],
     css: source_folder + '/scss/main.scss',
     js: source_folder + '/js/script.js',
-    img: source_folder + '/img/**/*.{jpg, png, svg, gif, ico, webp}',
+    img: source_folder + '/img/**/*.+(png|jpg|gif|ico|svg|webp)',
     fonts: source_folder + '/fonts/*.ttf',
   },
   watch: {
     html: source_folder + '/**/*.html',
     css: source_folder + '/scss/**/*.scss',
     js: source_folder + '/js/**/*.js',
-    img: source_folder + '/img/**/*.{jpg, png, svg, gif, ico, webp}',
+    img: source_folder + '/img/**/*.+(png|jpg|gif|ico|svg|webp)',
   },
   clean: "./" + project_folder + "/",
 }
