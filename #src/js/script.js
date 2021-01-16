79184cf5ae845441cdd0b69e4d6939269b8a2c70
AnastasiaPolkovnikova <68498874+AnastasiaPolkovnikova@users.noreply.github.com>
2021-01-16 14:54:29 +0800

    Update script.js

--- #src/js/script.js
+++ #src/js/script.js
@@ -1,20 +1,2 @@
-// @@include('gulpSetWebp');
-// import dinamicAdapt from './dynamicAdapt_dev';
-
-function testWebP(callback) {
-
-var webP = new Image();
-webP.onload = webP.onerror = function () {
-callback(webP.height == 2);
-};
-webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
-}
-
-testWebP(function (support) {
-
-if (support == true) {
-document.querySelector('body').classList.add('webp');
-}else{
-document.querySelector('body').classList.add('no-webp');
-}
-});
+@@include('gulpSetImgWebp.js');
+@@include('dynamicAdapt_dev.js');
